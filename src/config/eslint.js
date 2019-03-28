55243a872eba06e96f4ff507e2da25ae3585b282
Markus J. Wetzel <markuswetzel@gmx.net>
2019-03-28 10:59:34 +0100

    Update eslint config

--- src/config/eslint.js
+++ src/config/eslint.js
@@ -16,8 +16,6 @@ module.exports = {
   },
   plugins: ['flowtype'],
   rules: {
-    // airbnb config modifications
-
     // import
     'import/prefer-default-export': 'off', // conflict when there is only 1 action
 
@@ -26,7 +24,6 @@ module.exports = {
 
     // react
     'react/no-unknown-property': 'off',
-    'react/jsx-pascal-case': ['error', { allowAllCaps: true, ignore: ['_'] }], // can be removed after update to babel 7
     'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
   },
   settings: {
