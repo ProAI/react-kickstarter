c4c2f8264012d2c727245d624d39c8f9161f94ce
Markus J. Wetzel <markuswetzel@gmx.net>
2022-02-25 22:23:56 +0100

    Fix ignore paths

--- src/webpack/watchDevelopmentWebpack.js
+++ src/webpack/watchDevelopmentWebpack.js
@@ -9,17 +9,12 @@ const webpackConfigClient = require('../config/webpack-dev-client');
 const defaultConfig = {
   host: 'localhost',
   port: 8080,
-  styles: {
-    main: null,
-    desktop: null,
-    mobile: null,
-  },
   devServer: {
     host: 'localhost',
     port: 8081,
   },
   ignore: {
-    paths: /node_modules/,
+    paths: [/node_modules/],
   },
 };
 
